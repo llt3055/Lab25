df539fb9811de4ce1c64d20eb3dcc066a1b1b343
llt3055 <llt30555@gmail.com>
2026-04-06 04:34:12 +0800

    try to add sth

--- main.cpp
+++ main.cpp
@@ -8,7 +8,30 @@ int main() {
     list<string> l;
     set<string> s;
     string nm;
+
+    // read codes.txt into vector
+    auto start = high_resolution_clock::now();
+    ifstream fin1("codes.txt");
+    while (true) {
+        getline(fin1, nm);
+        v.push_back(nm);
+    }
+    fin1.close();
+    vRead = duration.count();
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<nanoseconds>(end - start);
     
+    // read codes.txt into list
+    start = high_resolution_clock::now();
+    ifstream fin2("codes.txt");
+    while (true) {
+        getline(fin2, nm);
+        l.push_back(nm);
+    }
+    fin2.close();
+    end = high_resolution_clock::now();
+    duration = duration_cast<nanoseconds>(end - start + 1);
+
     return 0;
 }
 
